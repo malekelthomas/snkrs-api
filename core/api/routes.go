8c5376947bbc4ea10d9425c05ecec68ac7bf6110
Hugh Thomas <hlawthomas@gmail.com>
2021-05-22 01:11:21 -0400

    Init modified routes

--- core/api/routes.go
+++ core/api/routes.go
@@ -1,17 +1,22 @@
 package api
 
 import (
+	"context"
+	"log"
 	"main/service"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-func main() {
-	service.Init()
+func Init() {
+	service.Init(context.TODO())
 	sneakerAPI := NewSneakerAPI(*service.AllServices.SneakerService)
+
 	r := mux.NewRouter()
-	r.HandleFunc("/sneakers", sneakerAPI.GetAllSneakers())
-	r.HandleFunc("sneakers/{sneaker}/", sneakerAPI.GetSneaker())
+	r.HandleFunc("/sneakers/", sneakerAPI.GetAllSneakers()).Methods("GET")
+	r.HandleFunc("/sneakers/", sneakerAPI.CreateSneaker()).Methods("POST")
+	r.HandleFunc("/sneakers/{sneaker}/", sneakerAPI.GetSneaker()).Methods("GET")
 	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
