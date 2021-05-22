e52130e1db6dc77989623ef62843bd1cbcfcef32
Hugh Thomas <hlawthomas@gmail.com>
2021-05-21 20:44:56 -0400

    added routes

--- core/api/routes.go
+++ core/api/routes.go
@@ -1,1 +1,17 @@
 package api
+
+import (
+	"main/service"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	service.Init()
+	sneakerAPI := NewSneakerAPI(*service.AllServices.SneakerService)
+	r := mux.NewRouter()
+	r.HandleFunc("/sneakers", sneakerAPI.GetAllSneakers())
+	r.HandleFunc("sneakers/{sneaker}/", sneakerAPI.GetSneaker())
+	http.Handle("/", r)
+}
