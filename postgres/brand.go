e85cb51ffc35519a00981c4d6ce03e8743452515
leke <malekelthomas97@gmail.com>
2021-06-09 19:18:27 -0400

    separated brands, price info

---
+++ postgres/brand.go
@@ -0,0 +1,30 @@
+package postgres
+
+import "context"
+
+func (s *Store) GetAllBrands(ctx context.Context) ([]string, error) {
+	//get values from db scan into store sneaker type
+	var brands []string
+	if err := s.DB.Select(&brands, `SELECT name FROM brands`); err != nil {
+		return nil, err
+	}
+
+	return brands, nil
+
+}
+
+func (s *Store) GetBrandByID(ctx context.Context, id int64) (string, error) {
+	var brand string
+	if err := s.DB.Get(&brand, `SELECT name FROM brands WHERE id=$1`, id); err != nil {
+		return "", err
+	}
+	return brand, nil
+}
+
+func (s *Store) GetBrandIDByName(ctx context.Context, name string) (string, error) {
+	var brand string
+	if err := s.DB.Get(&brand, `SELECT id FROM brands WHERE name=$1`, name); err != nil {
+		return "", err
+	}
+	return brand, nil
+}
---
+++ postgres/size_price.go
@@ -0,0 +1,34 @@
+package postgres
+
+import (
+	"context"
+	"database/sql"
+	"snkrs/pkg/domain"
+)
+
+func (s *Store) StoreSiteSizePrice(ctx context.Context, tx *sql.Tx, sneakerID, inventoryID int64, info *domain.SiteSizePrice) error {
+
+	if tx == nil {
+		var err error
+		tx, err = s.DB.Begin()
+		if err != nil {
+			return err
+		}
+	}
+	for site := range info.SitesSizesPrices {
+		var siteID int64
+		if err := s.DB.Get(&siteID, `SELECT id FROM sites WHERE site=$1`, site); err != nil {
+			return err
+		}
+		sizes_prices := info.SitesSizesPrices[site]
+		for size := range sizes_prices.SizesPrices {
+			price := sizes_prices.SizesPrices[size]
+			if _, err := tx.Exec(`INSERT INTO site_size_price (site_id, sneaker_id, size, price, inventory_id) VALUES ($1, $2, $3, $4, $5)`, siteID, sneakerID, size, price, inventoryID); err != nil {
+				tx.Rollback()
+				return err
+			}
+		}
+	}
+
+	return nil
+}
