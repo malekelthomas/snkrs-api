453cd5fc42819d94dee997fd8a14477edfa5ea8f
leke <malekelthomas97@gmail.com>
2021-06-10 00:07:56 -0400

    added function to return sites sizes prices map

--- postgres/size_price.go
+++ postgres/size_price.go
@@ -32,3 +32,48 @@ func (s *Store) StoreSiteSizePrice(ctx context.Context, tx *sql.Tx, sneakerID, i
 
 	return nil
 }
+
+type siteSizePriceRow struct {
+	id         int64  `db:"id"`
+	site_id    int64  `db:"site_id"`
+	sneaker_id int64  `db:"sneaker_id"`
+	size       string `db:"size"`
+	price      int64  `db:"price"`
+}
+
+func (s *Store) GetSitesSizesPrices(ctx context.Context, sneakerID int64) (*domain.SiteSizePrice, error) {
+	var info *domain.SiteSizePrice
+	var rows []siteSizePriceRow
+
+	//get all rows for this sneaker
+	if err := s.DB.Select(&rows, `SELECT * FROM site_size_price WHERE sneaker_id=$1`, sneakerID); err != nil {
+		return nil, err
+	}
+
+	//build out maps
+	for i := range rows {
+		row := rows[i]
+
+		//get site name
+		var site string
+		if err := s.DB.Get(&site, `SELECT name FROM sites WHERE id=$1`, row.site_id); err != nil {
+			return nil, err
+		}
+		//initialize sizes and prices
+		var sizePrice *domain.SizePrice
+
+		//check if size_price map already exists for site
+		if val, ok := info.SitesSizesPrices[site]; !ok {
+			//if not set it
+			info.SitesSizesPrices[site] = sizePrice
+		} else if ok {
+			//if it does set pointer to map
+			sizePrice = val
+		}
+		//add size price
+		sizePrice.SizesPrices[row.size] = row.price
+
+	}
+
+	return info, nil
+}
