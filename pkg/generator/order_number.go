2d0e0615933d3f90651f785903d586b2c764972a
leke <malekelthomas97@gmail.com>
2021-06-10 15:35:53 -0400

    removed init

--- pkg/generator/order_number.go
+++ pkg/generator/order_number.go
@@ -1,12 +1,10 @@
 package generator
 
 type OrderNumberGenerator interface {
-	Init()
 	New() (int64, error)
 }
 
 type NumberGenerator interface {
-	Init()
 	New() (int64, error)
 }
 
@@ -16,16 +14,6 @@ type orderNumberGenerator struct {
 
 func NewOrderNumberGenerator(n NumberGenerator) OrderNumberGenerator {
 	return &orderNumberGenerator{n}
-}
-
-func (o orderNumberGenerator) Init() {
-	/* var st sonyflake.Settings
-	sf = sonyflake.NewSonyflake(st)
-	if sf == nil {
-		panic("sonyflake not created")
-	} */
-
-	o.n.Init()
 }
 
 func (o orderNumberGenerator) New() (int64, error) {
