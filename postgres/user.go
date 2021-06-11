3913841f2c64f92fb2200452c9ec6d3934e1a7ff
leke <malekelthomas97@gmail.com>
2021-06-11 19:12:23 -0400

    role, auth

--- postgres/user.go
+++ postgres/user.go
@@ -7,18 +7,26 @@ import (
 
 func (s *Store) CreateUser(ctx context.Context, user *domain.User) (*domain.User, error) {
 	var userID int64
+
+	//get user role id
+	var uRoleID int64
+	if err := s.DB.Get(&uRoleID, `SELECT id FROM user_roles WHERE user_role=$1`, user.UserRole); err != nil {
+		return nil, err
+	}
+
 	if err := s.DB.QueryRow(`INSERT INTO users (
 			first_name,
 			last_name,
 			email,
-			password,
 			user_role_id,
 			auth_id
 		)
-		VALUES ($1, $2, $3, $4, $5, $6) RETURNING id`,
+		VALUES ($1, $2, $3, $4, $5) RETURNING id`,
 		user.FirstName,
 		user.LastName,
 		user.Email,
+		uRoleID,
+		user.AuthID,
 	).Scan(&userID); err != nil {
 		return nil, err
 	}
